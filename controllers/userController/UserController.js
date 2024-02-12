31327646d24666720024c3916091df47fd4f267c
Saheed Dammy <saidex1@ymail.com>
2024-02-12 23:03:42 +0100

    updated code

---
+++ controllers/userController/UserController.js
@@ -0,0 +1,17 @@
+import { request, response } from "express";
+
+const req = request
+const res = response
+
+class UserController {
+    
+    static async createUser(req, res){
+
+        try{
+            
+
+        } catch(error){
+
+        }
+    }
+}
