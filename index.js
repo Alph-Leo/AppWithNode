3ce2bff598726212d1e9738db95a9ea43e7ebfee
Saheed Dammy <saidex1@ymail.com>
2024-02-13 11:23:53 +0100

    updated code

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
+import { router } from './routes/userRoutes/UserRouter'
 
 dotenv.config()
 const port = process.env.PORT
 
 const mongoURL = process.env.DB_URL
-
-const app = express();
-
 mongoose.Promise = global.Promise;
 
 mongoose.connect(mongoURL, {
-    useNewUrlParser: true
-})
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
 
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", async () => {
+    console.log("Connected to MongoDB");
+});
+
+const app = express();
+
+app.use('/api/v1/diaryapp', router)
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
